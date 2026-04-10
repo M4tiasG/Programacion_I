83fb4e93ba79b4d2ec3767de0fadaa752cac25d5
M4tiasG <mataxd4@gmail.com>
2026-04-10 19:18:08 -0300

    Mejoras en la calculadora, Ejer 4

--- Python Ejercicios/ejer4_calculadora.py
+++ Python Ejercicios/ejer4_calculadora.py
@@ -1,9 +1,19 @@
 import time # Para el time.sleep
 
-def entradaNumeros(a,b):
-    a= float(input("Ingrese el primer número: "))
-    b= float(input("Ingrese el segundo número: "))
-    return a, b
+def entradaNumero(a):
+    while True:
+        a=input("Ingrese el número a ser operado: ")
+        
+        # Try convierte el valor ingresado en float, si
+        # es un número entoces sale del bucle, si es un string
+        # dará error y volverá a pedir una entrada válida.
+        try:
+            a = float(a)
+            break
+        except ValueError:
+            print("Por favor ingrese un número válido.")
+            print("")
+    return a
     
 def salidaNumeros(res):
     if res >= 0:
@@ -13,6 +23,7 @@ def salidaNumeros(res):
         print("")
     else:
         print("ERROR.")
+        print("Resultado negativo.")
         time.sleep(2.5)
 
 n1=0
@@ -30,23 +41,27 @@ while True:
     menu_opcion= int(input("Ingrese la operación a realizar: "))
     
     if menu_opcion == 1:
-        n1, n2 = entradaNumeros(n1,n2)
+        n1 = entradaNumero(n1)
+        n2= entradaNumero(n2)
         res= n1+n2
         salidaNumeros(res)
     elif menu_opcion == 2:
-        n1, n2 = entradaNumeros(n1,n2)
+        n1 = entradaNumero(n1)
+        n2= entradaNumero(n2)
         res= n1-n2
         salidaNumeros(res)
     elif menu_opcion == 3:
-        n1, n2 = entradaNumeros(n1,n2)
+        n1 = entradaNumero(n1)
+        n2= entradaNumero(n2)
         res= n1*n2
         salidaNumeros(res)
     elif menu_opcion == 4:
-        n1, n2 = entradaNumeros(n1,n2)
+        n1 = entradaNumero(n1)
+        n2= entradaNumero(n2)
         res= n1/n2
         salidaNumeros(res)
-    elif menu_opcion > 5:
-        print("ERROR.")
-        print("Ingrese una opción válida.")
     elif menu_opcion == 5:
         break
+    else:
+        print("ERROR.")
+        print("Ingrese una opción válida.")
